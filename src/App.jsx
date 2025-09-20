e8d6cb9923fb533f2e745295d5dc9b1e866037cb
Nicko-rgb <mancillanixon7@gmail.com>
2025-09-19 21:41:12 -0500

    creacion de modulos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './shared/styles/global.css';
+
+function App() {
+
+    return (
+        <div>
+            <h3>Hola Mundo</h3>
+        </div>
+    )
+}
+
+export default App
---
+++ src/modules/facilities/pages/FacilitiesPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/facilitiePage.css';
+
+const FacilitiesPage = () => {
+    return (
+        <div>FacilitiesPage</div>
+    )
+}
+
+export default FacilitiesPage
